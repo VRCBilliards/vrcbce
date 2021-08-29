f6a54d8e7bd833750d3b0ab7a88d6f776ab555cb
Metamaniac <masterfireheart@gmail.com>
2021-08-29 19:49:35 -0400

    The mega-commit of DOOM
    Verified Panda's work is valid while also making some more tweaks of my own on top of that. The main one being an attempt to make a cue-respawn button in case all else fails and players are unable to grab the cues (either because they're stuck inside the table's raycast-less colliders, or a wall somewhere).
    
    To be clear, this commit does not mean this branch's work is DONE, I just want to push everything I've done the past 2 days to at least keep things up to speed, will try and knock out the remaining blockers if I can the next few days

---
+++ Assets/VRCBilliardsCE/Scripts/CueRespawn.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace VRCBilliards {
+    public class CueRespawn : UdonSharpBehaviour
+    {
+        public PoolCue[] poolCues;
+        void Interact()
+        {
+            poolCues[0]._Respawn();
+            poolCues[1]._Respawn();
+        }
+    }
+}
