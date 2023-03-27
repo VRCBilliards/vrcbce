b7462a945652a2ba81359d2e4adbd60edb4b207b
Reava <93742413+Reava@users.noreply.github.com>
2023-03-28 01:41:54 +0200

    U# Assembly, Balls quality Switcher and fix for missing reference (#173)
    * Fix missing U# Assembly
    
    * Add balls quality switcher system to table core
    
    * Fix missing reference to table mesh
    
    * Fix pool cues not working in VR.
    
    A bool was incorrectly evaluated in PoolCue. This
    has been fixed.
    
    * Fix missing U# Assembly
    
    * Add balls quality switcher system to table core
    
    * Fix missing reference to table mesh
    
    * Make some code modifications to better match the repo.
    
    Generally, use #ifdefs to handle code routes, not bools set
    by #ifdefs. This makes the code path far clearer by stating
    intent as close to the reader's eye as possible.
    
    I also renamed a few vars and added a tooltip.
    
    ---------
    
    Co-authored-by: Hannah Giovanna Dawson <karakkaraz@gmail.com>

---
+++ Packages/com.vrcbilliards.vrcbce/Runtime/Scripts/PoolTableBallsSwitcher.cs
@@ -0,0 +1,109 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.Serialization;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class PoolTableBallsSwitcher : UdonSharpBehaviour
+{
+    [Header("HQ & LQ balls Mesh Renderers")]
+    [SerializeField] private MeshFilter[] balls;
+    [Space]
+    [SerializeField] private Mesh[] ballHQ;
+    [SerializeField] private Mesh[] ballLQ;
+    [FormerlySerializedAs("keepQuestOnLowQuality")]
+    [FormerlySerializedAs("kepQuestOnLowQuality")]
+    [Header("Defaults")]
+    [SerializeField, Tooltip("Should Quest only use low quality balls? This will save draw calls.")] private bool questLowQualityOnly;
+    [SerializeField] private bool defaultsHighQualityOnPC = true;
+    [SerializeField] private bool defaultsLowQualityOnQuest = true;
+    private bool isHighQuality = true;
+    
+    void Start()
+    {
+#if UNITY_ANDROID
+        if (!defaultsLowQualityOnQuest) {
+            return;
+        }
+#else
+        if (defaultsHighQualityOnPC)
+        {
+            return;
+        }
+#endif        
+        
+        _enableLQ();
+    }
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (Networking.LocalPlayer.playerId == player.playerId)
+        {
+            _enableHQ();
+        }
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (Networking.LocalPlayer.playerId == player.playerId)
+        {
+            _enableLQ();
+        }
+    }
+
+    public void _switchQuality()
+    {
+#if UNITY_ANDROID
+        if (questLowQualityOnly) {
+            return;
+        }
+#endif
+        if (isHighQuality)
+        {
+            _enableLQ();
+        }
+        else
+        {
+            _enableHQ();
+        }
+    }
+
+    public void _enableLQ()
+    {
+        for (int i = 0; i < 16; i++)
+        {
+            if(balls[i] != null)
+            {
+                if(ballLQ[i] != null)
+                {
+                    balls[i].mesh = ballLQ[i];
+                }
+            }
+        }
+        
+        isHighQuality = false;
+    }
+
+    public void _enableHQ()
+    {
+        #if UNITY_ANDROID
+        if (questLowQualityOnly)
+        {
+            return;
+        }
+        #endif
+        
+        for (int i = 0; i < 16; i++)
+        {
+            if (balls[i] != null)
+            {
+                if (ballHQ[i] != null)
+                {
+                    balls[i].mesh = ballHQ[i];
+                }
+            }
+        }
+        
+        isHighQuality = true;
+    }
+}
